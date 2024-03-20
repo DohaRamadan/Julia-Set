c8681d67b0f3cb83c53e6e18416f4b8b205fefe4
DohaRamadan <77820526+DohaRamadan@users.noreply.github.com>
2024-03-20 16:16:29 +0200

    added julia set

--- sketch.js
+++ sketch.js
@@ -1,25 +1,38 @@
-var frDiv;
-
+let mnVal = -2.5;
+let maxVal = 2.5;
+let angle = 0; 
+let maxIteration = 100;
+const colorsRed = []; 
+const colorsGreen = []; 
+const colorsBlue = []; 
 function setup() {
-    createCanvas(window.innerWidth - 100, window.innerHeight - 100);
-    pixelDensity(1);
-  
-      minSlider = createSlider(-2.5, 0, -2.5, 0.01);
-      maxSlider = createSlider(0, 2.5, 2.5, 0.01);
-  
-    frDiv = createDiv('');
+  pixelDensity(1);
+  createCanvas(window.innerWidth, window.innerHeight);
+  colorMode(HSB, 1);
+
+  for (let n = 0; n < maxIteration; n++) {
+    let hu = sqrt(n / maxIteration);
+    let col = color(hu, 255, 150);
+    colorsRed[n] = red(col);
+    colorsGreen[n] = green(col);
+    colorsBlue[n] = blue(col);
+    
+  }
 }
 
 function draw() {
+  background(255);
+
   loadPixels();
-  let maxIteration = 100;
+  let ca = cos(angle * 3.213); 
+  let cb = sin(angle); 
+
+  angle += 0.02; 
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
-      let a = map(x, 0, width, minSlider.value(), maxSlider.value());
-      let b = map(y, 0, height, minSlider.value(), maxSlider.value());
-    
-      let ca = a; 
-      let cb = b; 
+      let a = map(x, 0, width, mnVal, maxVal);
+      let b = map(y, 0, height, mnVal, maxVal);
+
       let numberOfIterations = 0;
       while (numberOfIterations < maxIteration) {
         let realPart = a * a - b * b;
@@ -28,24 +41,25 @@ function draw() {
         a = realPart + ca;
         b = imaginaryPart + cb;
 
-        if (a*a + b*b > 16) {
+        if (a * a + b * b > 16) {
           break;
         }
         numberOfIterations++;
       }
-      let bright = map(numberOfIterations, 0, maxIteration, 0, 1);
-      bright = map(sqrt(bright), 0, 1, 0, 255); 
+      let hue = sqrt(numberOfIterations / maxIteration);
+      let col = color(hue, 255, 255);
 
+      let pixelIndex = (y * width + x) * 4;
       if (numberOfIterations === maxIteration) {
-        bright = 0;
+        pixels[pixelIndex + 0] = 0;
+        pixels[pixelIndex + 1] = 0;
+        pixels[pixelIndex + 2] = 0;
+      } else {
+        pixels[pixelIndex + 0] = colorsRed[numberOfIterations];
+        pixels[pixelIndex + 1] = colorsGreen[numberOfIterations];
+        pixels[pixelIndex + 2] = colorsBlue[numberOfIterations];
       }
-      let pixelIndex = ((y * width) + x) * 4; 
-      pixels[pixelIndex + 0] = bright; 
-      pixels[pixelIndex + 1] = bright; 
-      pixels[pixelIndex + 2] = bright; 
-      pixels[pixelIndex + 3] = 255; 
     }
   }
-  updatePixels(); 
-  frDiv.html(floor(frameRate()));
+  updatePixels();
 }
